3d38d5aade52a98065cd2d0f9f80ca19be9eb47d
dodinovembri <dodinovembri32@gmail.com>
2023-01-05 00:35:39 +0700

    update

--- app/Models/BIllModel.php
+++ app/Models/BIllModel.php
@@ -6,5 +6,5 @@ use CodeIgniter\Model;
 class BillModel extends Model {
     protected $table = 'bill';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['status', 'created_at', 'modified_at', 'creator_id', 'modifier_id', 'due_date', 'total', 'to_account_number', 'description'];
+    protected $allowedFields = ['status', 'created_at', 'modified_at', 'creator_id', 'modifier_id', 'due_date', 'total', 'to_account_number', 'description']; 
 }
